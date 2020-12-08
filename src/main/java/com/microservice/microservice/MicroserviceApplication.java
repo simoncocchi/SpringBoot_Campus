8d45a4edbb190d30b8f188e7daba8359dc88a84d
COCCHI Simon <59911613+simoncocchi@users.noreply.github.com>
2020-12-08 13:36:45 +0100

    [Créer une API REST avec SpingBoot] Etapes 3 (mise en place de swagger)

--- src/main/java/com/microservice/microservice/MicroserviceApplication.java
+++ src/main/java/com/microservice/microservice/MicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
